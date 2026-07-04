a9bdceb68f60344013b19d65be51a1021e7f55cd
naufalahmdd <naufaldev10@gmail.com>
2026-07-04 15:06:02 +0700

    feat(android): initialize MainActivity method channel setup for native logic

--- android/app/src/main/kotlin/com/example/diginews/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/diginews/MainActivity.kt
@@ -1,5 +1,29 @@
 package com.example.diginews
 
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "com.diginews/nim_reverser"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "reverseNIM") {
+                val nim = call.argument<String>("nim")
+                if (nim != null) {
+                    // Logika membalikkan string NIM menggunakan fungsi bawaan Kotlin
+                    val reversedNIM = nim.reversed()
+                    result.success(reversedNIM)
+                } else {
+                    result.error("INVALID_ARGUMENT", "NIM tidak boleh kosong", null)
+                }
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+}
